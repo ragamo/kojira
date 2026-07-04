779697b00acbce9df91b24ba49e0e193ad899666
Christian Haack <christian.haack@latam.com>
2026-07-04 16:11:10 -0400

    Clear transitions list after applying to prevent stale dropdown on reopen

--- src/app.rs
+++ src/app.rs
@@ -968,6 +968,7 @@ impl App {
                         let key = issue.key.clone();
                         issue.fields.status.name = transition.to.name.clone();
                         self.detail_transition_open = false;
+                        self.detail_transitions.clear();
                         self.do_transition(&key, &transition.id);
                     }
                 }
@@ -2050,6 +2051,7 @@ impl App {
                                     let key = issue.key.clone();
                                     issue.fields.status.name = transition.to.name.clone();
                                     self.detail_transition_open = false;
+                                    self.detail_transitions.clear();
                                     self.do_transition(&key, &transition.id);
                                 }
                             }
